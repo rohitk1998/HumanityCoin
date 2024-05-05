411b587874e6e182bd991e5151eb37c39b082339
ravikumar26-jangir <156072022+Rkjangir26@users.noreply.github.com>
2024-05-05 22:52:54 +0530

    coinmarketcap api updated

--- src/Components/Pages/Explorer/ExpTokenTable.jsx
+++ src/Components/Pages/Explorer/ExpTokenTable.jsx
@@ -2,152 +2,172 @@ import React, { useState, useEffect } from "react";
 import { Table } from "antd";
 import "./Explore.scss";
 import fetch from 'node-fetch'; // Import node-fetch
-import Moralis from 'moralis';
 
-const jsonData = [
-  {
-      "ticker": "USDC",
-      "img": "https://cdn.moralis.io/eth/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
-      "name": "USD Coin",
-      "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-      "decimals": 6
-  },
-  {
-      "ticker": "LINK",
-      "img": "https://cdn.moralis.io/eth/0x514910771af9ca656af840dff83e8264ecf986ca.png",
-      "name": "Chainlink",
-      "address": "0x514910771af9ca656af840dff83e8264ecf986ca",
-      "decimals": 18
-  },
-  {
-      "ticker": "USDT",
-      "img": "https://cdn.moralis.io/eth/0xdac17f958d2ee523a2206206994597c13d831ec7.png",
-      "name": "Tether USD",
-      "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
-      "decimals": 6
-  },
-  {
-      "ticker": "GUSD",
-      "img": "https://cdn.moralis.io/eth/0x056fd409e1d7a124bd7017459dfea2f387b6d5cd.png",
-      "name": "Gemini USD",
-      "address": "0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd",
-      "decimals": 2
-  },
-  {
-      "ticker": "DAI",
-      "img": "https://cdn.moralis.io/eth/0x6b175474e89094c44da98b954eedeac495271d0f.png",
-      "name": "Dai Stablecoin",
-      "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
-      "decimals": 18
-  },
-  {
-      "ticker": "WETH",
-      "img": "https://cdn.moralis.io/eth/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
-      "name": "Wrapped Ethereum",
-      "address": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-      "decimals": 18
-  },
-  {
-      "ticker": "WBTC",
-      "img": "https://cdn.moralis.io/eth/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png",
-      "name": "Wrapped Bitcoin",
-      "address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
-      "decimals": 8
-  },
-  {
-      "ticker": "MATIC",
-      "img": "https://cdn.moralis.io/eth/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png",
-      "name": "Matic Token",
-      "address": "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
-      "decimals": 18
-  },
-  {
-      "ticker": "UNI",
-      "img": "https://cdn.moralis.io/eth/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984.png",
-      "name": "Uniswap",
-      "address": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
-      "decimals": 18
-  },
-  {
-      "ticker": "CRV",
-      "img": "https://cdn.moralis.io/eth/0xd533a949740bb3306d119cc777fa900ba034cd52.png",
-      "name": "Curve DAO Token",
-      "address": "0xd533a949740bb3306d119cc777fa900ba034cd52",
-      "decimals": 18
-  },
-  {
-      "ticker": "MKR",
-      "img": "https://cdn.moralis.io/eth/0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2.png",
-      "name": "Maker",
-      "address": "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
-      "decimals": 18
-  },
-  {
-      "ticker": "SHIB",
-      "img": "https://cdn.moralis.io/eth/0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce.png",
-      "name": "Shiba Inu",
-      "address": "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce",
-      "decimals": 18
-  },
-  {
-      "ticker": "AAVE",
-      "img": "https://cdn.moralis.io/eth/0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9.png",
-      "name": "AAVE",
-      "address": "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
-      "decimals": 18
-  }
+const tokenImagesArray=
+    [
+        {
+            "name": "Bitcoin",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
+        },
+        {
+            "name": "Ethereum",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
+        },
+        {
+            "name": "Tether USDt",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png"
+        },
+        {
+            "name": "BNB",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png"
+        },
+        {
+            "name": "Solana",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png"
+        },
+        {
+            "name": "USDC",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png"
+        },
+        {
+            "name": "XRP",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/52.png"
+        },
+        {
+            "name": "Dogecoin",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/74.png"
+        },
+        {
+            "name": "Toncoin",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/2642.png"
+        },
+        {
+            "name": "Cardano",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png"
+        },
+        {
+            "name": "Shiba Inu",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/5994.png"
+        },
+        {
+            "name": "Avalanche",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/5805.png"
+        },
+        {
+            "name": "TRON",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/1958.png"
+        },
+        {
+            "name": "Polkadot",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png"
+        },
+        {
+            "name": "Bitcoin Cash",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/1831.png"
+        },
+        {
+            "name": "Chainlink",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/1975.png"
+        },
+        {
+            "name": "NEAR Protocol",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/6535.png"
+        },
+        {
+            "name": "Polygon",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png"
+        },
+        {
+            "name": "Litecoin",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/2.png"
+        },
+        {
+            "name": "Internet Computer",
+            "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/8916.png"
+        },
+        
+            {    "name": "UNUS SED LEO",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/3957.png"
+            },
+            {    "name": "Dai",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png"
+            },
+            {   "name": "Uniswap",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png"
+            },
+            {   "name": "Ethereum Classic",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/1321.png"
+            },
+            {   "name": "Aptos",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/4642.png"
+            },
+            {   "name": "Hedera",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/21794.png"
+            },
+            {   "name": "First Digital USD",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/26081.png"
+            },
+            {   "name": "Render",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/5690.png"
+            },
+            {   "name": "Pepe",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/24478.png"
+            },
+            {   "name": "Cosmos",
+                "image_link": "https://s2.coinmarketcap.com/static/img/coins/64x64/3635.png"
+            }
+        
+    
+    
 
+];
+// Convert the array to an object
+const tokenImages = tokenImagesArray.reduce((acc, token) => {
+    acc[token.name] = token.image_link;
+    return acc;
+}, {});
 
-]
-
-
-
-
-
-
+console.log(tokenImages);
 
 const ExpTokenTable = () => {
     const [tokenData, setTokenData] = useState([]);
+    const apikey = '2ad9f937-58a4-458b-8613-72a2dc7350ef';
 
     useEffect(() => {
-        fetchLivePrices();
-    }, []);
+        const fetchData = () => {
+            if (!apikey) {
+                console.error("API key is undefined. Cannot fetch data.");
+                return;
+            }
 
-    const fetchLivePrices = async () => {
-        try {
-            const options = {
-                method: 'POST',
-                headers: {
-                    accept: 'application/json',
-                    'content-type': 'application/json',
-                    'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImI5NmZjNWRlLWY4MTAtNGYxNi1hNDYyLTFkOWRmNmZkNzU2YiIsIm9yZ0lkIjoiMzgxOTcyIiwidXNlcklkIjoiMzkyNDg2IiwidHlwZSI6IlBST0pFQ1QiLCJ0eXBlSWQiOiI2NTQ3YzBlMy0zNzRlLTRmOWMtOTFkNS00Mzg4NmU2YTBiMTciLCJpYXQiOjE3MTM4ODkyMzMsImV4cCI6NDg2OTY0OTIzM30.ip3q7OFGjMZp6Cq4GGBji4Do-i75de6PJsoTisKKmd4' // Replace with your Moralis API key
-                },
-                body: JSON.stringify({
-                    "tokens": jsonData.map(item => ({ "token_address": item.address }))
-                })
-            };
+            fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${apikey}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data from CoinMarketCap API");
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data); // Log the raw data from the API
+                const sortedCryptos = data.data.sort((a, b) => {
+                    const aMarketCap = a.quote && a.quote.USD ? a.quote.USD.market_cap : 0;
+                    const bMarketCap = b.quote && b.quote.USD ? b.quote.USD.market_cap : 0;
+                    return bMarketCap - aMarketCap;
+                });
+                const topN = 30;
+                const topCryptos = sortedCryptos.slice(0, topN);
+                setTokenData(topCryptos);
+                console.log(topCryptos); // Log the sorted and sliced data
+            }).catch(error => {
+                console.error("Error fetching data:", error);
+            });
+        };
 
-            const response = await fetch('https://deep-index.moralis.io/api/v2.2/erc20/prices?chain=eth&include=percent_change', options);
-            const data = await response.json();
+        fetchData();
+        const interval = setInterval(fetchData, 2000); // Call every 5 seconds
+      return () => clearInterval(interval); // Cleanup
 
-            console.log('Live Prices Response:', data);
-
-            // Update token data with live prices
-  
-            const updatedData = jsonData.map((item, index) => ({
-                ...item,
-                price: data[index]?.usdPrice || "-",
-                hourChange: data[index]?.priceChange1h || "-",
-                dayChange: data[index]?.['24hrPercentChange'] || "-",
-                fdv:data[index]?.priceChange1h || "-",
-                volume: data[index]?.priceChange1h || "-",
-            }));
-
-            setTokenData(updatedData);
-        } catch (error) {
-            console.error('Error fetching live prices:', error);
-        }
-    };
+    }, [apikey]); // Depend on apikey to re-fetch if it changes
 
     const columns = [
         {
@@ -157,75 +177,85 @@ const ExpTokenTable = () => {
         },
         {
             title: "Token Name",
-            dataIndex: "tokenName",
-            key: "tokenName",
+            dataIndex: "name",
+            key: "name",
             render: (text, record) => (
                 <p className="hashdata">
-                   <img src={record.img} alt="Token" style={{ width: "55px", height: "55px" }} />
+                   <img src={tokenImages[record.name] || 'default_image_path.png'} alt="Token" style={{ width: "55px", height: "55px" }} />
                     {record.name} 
                 </p>
             ),
         },
         {
-            title: "Price (USD)",
+            title: "Price ",
             dataIndex: "price",
             key: "price",
             render: (text, record) => (
-              <p className="hashdata">
-                 {console.log(record)}
-                  {record.price}
-              </p>
-          ),
-            
-
+                <p className="hashdata">
+                    ${record.price.toFixed(3)}
+                </p>
+            ),
         },
         {
-            title: "1 Hour Change",
+            title: "1h %",
             dataIndex: "hourChange",
             key: "hourChange",
-            render: (text, record) => (
-              <p className="hashdata">
-                 {console.log(record)}
-                  {record.hourChange} 
-              </p>
-        ),
+            render: (text, record) => {
+                const changeClass = record.hourChange >= 0 ? "positive-change" : "negative-change";
+                return (
+                    <p className={`hashdata ${changeClass}`}>
+                        {record.hourChange.toFixed(2)}%
+                    </p>
+                );
+            },
         },
         {
-            title: "1 Day Change",
+            title: "24 %",
             dataIndex: "dayChange",
             key: "dayChange",
-            render: (text, record) => (
-              <p className="hashdata">
-                 {console.log(record)}
-                  {record.dayChange} 
-              </p>
-        ),
-      },
+            render: (text, record) => {
+                const changeClass = record.dayChange >= 0 ? "positive-change" : "negative-change";
+                return (
+                    <p className={`hashdata ${changeClass}`}>
+                        {record.dayChange.toFixed(2)}%
+                    </p>
+                );
+            },
+        },
         {
-            title: "FDV",
+            title: "FDMC",
             dataIndex: "fdv",
             key: "fdv",
+            render: (text, record) => (
+                <p className="hashdata">
+                    ${(record.fdv / 1000000000).toFixed(2)}B
+                </p>
+            ),
         },
         {
-            title: "Volume",
+            title: "Volume(24h)",
             dataIndex: "volume",
             key: "volume",
+            render: (text, record) => (
+                <p className="hashdata">
+                    ${(record.volume / 1000000).toFixed(2)}M
+                </p>
+            ),
         },
-       
     ];
 
     const mappedData = tokenData.map((item, index) => ({
         key: `${index}`,
         hash: index + 1,
-        img: item.img,
+        img: item.logo, // Assuming 'logo' is the correct property for the image URL
         name: item.name,
-        ticker: item.ticker,
-        price: item.price,
-        hourChange: item.hourChange,
-        dayChange: item.dayChange,
-        fdv: item.fdv,
-        volume: item.volume,
-        status: item.status,
+        ticker: item.symbol, // Assuming 'symbol' is the correct property for the ticker
+        price: item.quote.USD.price, // Accessing price from the 'quote' object
+        hourChange: item.quote.USD.percent_change_1h, // Assuming you want the 1-hour percentage change
+        dayChange: item.quote.USD.percent_change_24h, // Assuming you want the 24-hour percentage change
+        fdv: item.quote.USD.fully_diluted_market_cap, // Assuming 'circulating_supply' is what you mean by 'FDV'
+        volume: item.quote.USD.volume_24h, // Assuming you want the 24-hour trading volume
+        status: item.status, // Assuming 'status' is a valid property
     }));
 
     return (
