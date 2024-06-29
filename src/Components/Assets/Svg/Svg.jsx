123a198f7d017ce71b09164975ede3e00f4c720e
Rohit Kumar <rohit.kumar@antiersolutions.com>
2024-06-29 16:48:25 +0530

    june 29 code pushed and working on testing of add liquidity and migrate module - full day

--- src/Components/Assets/Svg/Svg.jsx
+++ src/Components/Assets/Svg/Svg.jsx
@@ -197,8 +197,8 @@ export function FacebookGrp() {
   return (
     <>
       <svg
-        width="41"
-        height="41"
+        width="36"
+        height="36"
         viewBox="0 0 41 41"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
